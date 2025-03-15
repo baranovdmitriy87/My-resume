ec7fd24528fdefe000d1fd36a4dd720f5ebb6a63
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2025-03-15 15:57:02 +0300

    Делает реализацию бургерного меню для мобильных устройств и планшетов

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 // You can add JavaScript for smooth scroll or form validation if needed
-document.querySelectorAll('.nav-links a').forEach(anchor => {
+document.querySelectorAll('.nav-site a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -7,3 +7,19 @@ document.querySelectorAll('.nav-links a').forEach(anchor => {
         });
     });
 });
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
